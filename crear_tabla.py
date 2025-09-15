600ba235a3ffa4aecd99346d0f33bf907283a52a
Valentina Gesto <128436260+valugen@users.noreply.github.com>
2025-09-15 20:48:47 -0300

    Update crear_tabla.py

--- crear_tabla.py
+++ crear_tabla.py
@@ -6,7 +6,7 @@ import sqlite3
 conexion = sqlite3.connect("contactos.db")
 cursor = conexion.cursor()
 
-#sentencia DDL para crear tabla
+#sentencia DDL para crear tabla contactos
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Contactos (
     id_contacto INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,7 +17,19 @@ CREATE TABLE IF NOT EXISTS Contactos (
 )
 """)
 
+#sentencia DDL para crear tabla contactos
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Factura (
+    id_factura INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_contacto INTEGER NOT NULL,
+    monto REAL NOT NULL,
+    fecha TEXT NOT NULL,
+    FOREIGN KEY (id_contacto) REFERENCES Contactos (id_contacto)
+)
+""")
+
 conexion.commit()
 conexion.close()
 
 print("Tabla Contactos creada correctamente")
+
